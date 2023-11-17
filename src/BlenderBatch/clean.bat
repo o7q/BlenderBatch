b765103187761506f3669b10371fa7e7cd1f4900
o7q <jev.o7q@gmail.com>
2023-11-17 02:36:16 -0600

    Delete clean.bat

--- src/BlenderBatch/clean.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rmdir ".vs" /s /q 2> nul
-rmdir "x64" /s /q 2> nul
-rmdir "BlenderBatch\BlenderBatch" /s /q 2> nul
-rmdir "BlenderBatch\x64" /s /q 2> nul
-del "BlenderBatch\BlenderBatch.aps" /f 2> nul
